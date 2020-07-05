2b2e94015d69579e40d9f4da0fa1050ff57ff396
Valentina Fadina <vafa1019@netcracker.com>
2020-07-05 13:33:13 +0300

    url shortener commit

---
+++ src/main/java/com/webapp/urlshortener/config/SpringRedisConfig.java
@@ -0,0 +1,31 @@
+package com.webapp.urlshortener.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.net.UnknownHostException;
+
+@Configuration
+public class SpringRedisConfig {
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
+        connectionFactory.setHostName("localhost");
+        connectionFactory.setPort(6379);
+        return connectionFactory;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(name = "redisTemplate")
+    public RedisTemplate<Object, Object> redisTemplate(
+            RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
--- src/main/java/com/webapp/urlshortener/controller/URLShortenerController.java
+++ src/main/java/com/webapp/urlshortener/controller/URLShortenerController.java
@@ -1,5 +1,9 @@
 package com.webapp.urlshortener.controller;
+import com.webapp.urlshortener.config.SpringRedisConfig;
 import org.apache.commons.validator.routines.UrlValidator;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 import com.google.common.hash.Hashing;
 
@@ -12,19 +16,32 @@ import java.nio.charset.StandardCharsets;
 @RestController
 @RequestMapping("/")
 public class URLShortenerController {
+    private static final String REDIS_TEMPLATE = "redisTemplate";
 
     @PostMapping()
     public String create(String url) {
         UrlValidator urlValidator = new UrlValidator(new String[]{"http", "https"});
         if (urlValidator.isValid(url)) {
-            return Hashing.murmur3_32().hashString(url, StandardCharsets.UTF_8).toString();
+            ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(SpringRedisConfig.class);
+            String uuid = Hashing.murmur3_32().hashString(url, StandardCharsets.UTF_8).toString();
+            RedisTemplate redisTemplate = (RedisTemplate) ctx.getBean(REDIS_TEMPLATE);
+            redisTemplate.opsForValue().set(uuid, url);
+            return uuid;
         }
 
         throw new RuntimeException("URL Invalid: " + url);
     }
 
-    @GetMapping()
-    public String get(String url) {
+    @GetMapping("/{id}")
+    public String get(@PathVariable String id) {
+        ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(SpringRedisConfig.class);
+        RedisTemplate redisTemplate = (RedisTemplate) ctx.getBean(REDIS_TEMPLATE);
+        String url = redisTemplate.opsForValue().get(id).toString();
+        System.out.println("URL Retrieved: " + url);
+
+        if (url == null) {
+            throw new RuntimeException("There is no shorter URL for : " + id);
+        }
         return url;
     }
 }
